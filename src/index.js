4ec0fa467c15896ca26262c91cd79b5cb47d657c
C.T. Lin <chentsulin@gmail.com>
2016-10-05 10:32:24 +0800

    createQueryInterfaceSpy -> createQueryInterfaceMock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { default as sequelize } from './sequelize';
 export {
   methods as queryInterfaceMethods,
-  createSpyInstance as createQueryInterfaceSpy,
+  createMockInstance as createQueryInterfaceMock,
 } from './query-interface';
 export { default as field } from './field';
